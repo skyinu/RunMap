6bd73dd89a99314f412eec5e036c34f564b3096b
getletcodes <1062860950@qq.com>
2017-06-19 19:19:49 +0800

    [Feature]import butterknife and test it at CalorieActivity

--- app/src/main/java/com/stdnull/runmap/ui/activity/CalorieActivity.java
+++ app/src/main/java/com/stdnull/runmap/ui/activity/CalorieActivity.java
@@ -1,56 +1,44 @@
 package com.stdnull.runmap.ui.activity;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.stdnull.runmap.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 
 /**
  * 距离转卡路里页面
  * Created by chen on 2017/2/14.
  */
 
-public class CalorieActivity extends BaseActivity implements TextWatcher{
-    private EditText mEditWeight;
-    private EditText mEditDistance;
-    private TextView mTvCalorie;
+public class CalorieActivity extends BaseActivity{
+    @BindView(R.id.edit_weight)
+    protected EditText mEditWeight;
+    @BindView(R.id.edit_distance)
+    protected EditText mEditDistance;
+    @BindView(R.id.text_calorie)
+    protected TextView mTvCalorie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calorie);
-        initView();
+        ButterKnife.bind(this);
     }
 
-    private void initView() {
-        mEditDistance = (EditText) findViewById(R.id.edit_distance);
-        mEditWeight = (EditText) findViewById(R.id.edit_weight);
-        mTvCalorie = (TextView) findViewById(R.id.text_calorie);
-        mEditWeight.addTextChangedListener(this);
-        mEditDistance.addTextChangedListener(this);
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
+    @OnTextChanged(value = {R.id.edit_weight, R.id.edit_distance},
+            callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    public void afterTextChanged(CharSequence text) {
         String distance = mEditDistance.getText().toString();
         String weight = mEditWeight.getText().toString();
         try{
             float dis = Float.valueOf(distance);
             float wei = Float.valueOf(weight);
             float res = dis * wei * 1.036F;
-            mTvCalorie.setText(res+"");
+            mTvCalorie.setText(res + "");
         }catch (Exception e){
 
         }
