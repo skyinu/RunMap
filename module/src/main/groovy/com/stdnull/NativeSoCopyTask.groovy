e52678c1ffeb0db5e8894ad6912e933a04c02837
getletcodes <1062860950@qq.com>
2017-08-17 00:15:13 +0800

    lack shell script

--- module/src/main/groovy/com/stdnull/NativeSoCopyTask.groovy
+++ module/src/main/groovy/com/stdnull/NativeSoCopyTask.groovy
@@ -6,11 +6,10 @@ import org.gradle.api.tasks.TaskAction
 /**
  * Created by chen on 2017/8/15.
  */
-class NativeSoCopyTask extends DefaultTask{
-    String group = "simplify"
-
+class NativeSoCopyTask extends DefaultTask {
+    String group = "simplify";
     @TaskAction
-    def copy(){
+    def copy() {
         println "start copy native so library"
         def sourceNativeLibDir = getProject().rootProject.rootDir.absolutePath + File.separator + "app" +
                 File.separator + "src" + File.separator + "main" + File.separator + "libs" +
@@ -21,10 +20,34 @@ class NativeSoCopyTask extends DefaultTask{
         if (checkIfNeedCopylibs("libconfig.so", sourceNativeLibDir, destNativeLibDir)) {
             copyLibs("libconfig.so", sourceNativeLibDir, destNativeLibDir)
             println "copy succeed"
+        } else {
+            try {
+                executeNativeBuild {
+                    copyLibs("libconfig.so", sourceNativeLibDir, destNativeLibDir)
+                }
+            } catch (Exception ex) {
+                println "copy failed " + ex
+            }
+        }
+    }
+
+    def executeNativeBuild(Closure callback) {
+        String sourceNativeLibDir = getProject().rootProject.rootDir.absolutePath + File.separator + "app" +
+                File.separator + "src" + File.separator + "main" + File.separator + "jni"
+        String cmdDir = getProject().rootProject.rootDir.absolutePath + File.separator + "gradle"+
+                File.separator + "command" + File.separator
+        String cmd = ""
+        println "cmdDir is "+cmdDir
+        if(System.getProperty("os.name").contains("Windows")){
+            cmd = "cmd /c " + cmdDir + "sh-ndk-build.bat " + sourceNativeLibDir
         }
         else{
-            println "copy failed"
+
         }
+        println "cmd is "+cmd
+        cmd.execute().waitFor()
+        callback()
+
     }
 
     def copyLibs(libName, sourceDir, destDir) {
@@ -38,11 +61,14 @@ class NativeSoCopyTask extends DefaultTask{
     }
 
     def checkIfNeedCopylibs(libName, sourceDir, destDir) {
-        def sourceFile = new File(sourceDir + File.separator + libName);
-        def destFile = new File(destDir + File.separator + libName);
+        def sourceFile = new File(sourceDir + File.separator + libName)
+        def destFile = new File(destDir + File.separator + libName)
         println sourceFile.absolutePath + " vs " + destFile.absolutePath
+        if(!sourceFile.exists()){
+            return false
+        }
         if (!destFile.exists()) {
-            return true;
+            return true
         }
         return sourceFile.lastModified() > destFile.lastModified()
     }
