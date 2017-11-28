9fc70f96f6968a730a6769533e00086dcbc41a2a
getletcodes <1062860950@qq.com>
2017-11-28 21:57:27 +0800

    bug fix

--- buildSrc/src/main/groovy/com/stdnull/MethodCallLogTransform.groovy
+++ buildSrc/src/main/groovy/com/stdnull/MethodCallLogTransform.groovy
@@ -1,4 +1,3 @@
-import com.android.build.api.transform.DirectoryInput
 import com.android.build.api.transform.Format
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
@@ -7,33 +6,55 @@ import com.android.build.api.transform.TransformInvocation
 import com.android.build.api.transform.TransformOutputProvider
 import com.android.build.gradle.internal.pipeline.TransformManager
 import com.google.common.collect.ImmutableSet
+import javassist.ClassPath
+import javassist.CtMethod
 import javassist.bytecode.AccessFlag
 import org.apache.commons.io.FileUtils
 import com.stdnull.logger.LoggerWrapper
-import javassist.ClassClassPath
 import javassist.ClassPool
 import javassist.CtClass
-import org.apache.tools.ant.util.StringUtils
 import org.gradle.api.Project
-import java.util.jar.JarEntry
-import java.util.jar.JarFile
+import org.gradle.api.tasks.compile.JavaCompile
 
 /**
  * Created by chen on 2017/11/26.
  */
 
-class MethodCallLogTransform extends Transform{
+class MethodCallLogTransform extends Transform {
     Project project
     LoggerWrapper loggerWrapper
     ClassPool classPool = ClassPool.getDefault()
+    List<String> classPathList
     File logClassMap
 
 
-    MethodCallLogTransform(Project project, LoggerWrapper loggerWrapper){
+    MethodCallLogTransform(Project project, LoggerWrapper loggerWrapper) {
         this.project = project
         this.loggerWrapper = loggerWrapper
-        this.logClassMap = new File(project.buildDir.absolutePath + File.separator + "log_mapping.txt")
+        this.logClassMap = new File(project.buildDir.absolutePath +
+                File.separator + "log_mapping.txt")
+        if(logClassMap.exists()){
+            logClassMap.delete()
+            logClassMap.createNewFile()
+        }
+        project.afterEvaluate {
+            handleClassPath()
+        }
     }
+
+    def handleClassPath(){
+        classPathList = new ArrayList<>()
+        if(!project.plugins.hasPlugin("com.android.library")){
+            return
+        }
+        project.android.libraryVariants.all {variant ->
+            JavaCompile javaCompile = variant.getJavaCompiler()
+            javaCompile.classpath.each {
+                classPathList.add(it.path)
+            }
+        }
+    }
+
     @Override
     String getName() {
         return "methodCallLog"
@@ -46,11 +67,9 @@ class MethodCallLogTransform extends Transform{
 
     @Override
     Set<? super QualifiedContent.Scope> getScopes() {
-        if(project.plugins.hasPlugin("com.android.library")){
-            loggerWrapper.error("library plugin")
+        if (project.plugins.hasPlugin("com.android.library")) {
             return ImmutableSet.of(QualifiedContent.Scope.PROJECT)
         }
-//        loggerWrapper.error("application plugin")
         return TransformManager.SCOPE_FULL_PROJECT
     }
 
@@ -58,76 +77,84 @@ class MethodCallLogTransform extends Transform{
     void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
         super.transform(transformInvocation)
         loggerWrapper.error("method call log transform run")
-        classPool.appendSystemPath()
         classPool.insertClassPath(project.android.bootClasspath[0].toString())
+        List<ClassPath> classPaths = new ArrayList<>()
+        classPathList.each {
+            try {
+                classPaths.add(classPool.insertClassPath(it))
+            }
+            catch (Exception e){
+                loggerWrapper.error(e.toString())
+            }
+        }
 
         TransformOutputProvider outputProvider = transformInvocation.outputProvider
-        if(transformInvocation.isIncremental()){
+        if (transformInvocation.isIncremental()) {
             outputProvider.deleteAll()
         }
         transformInvocation.inputs.each {
             it.jarInputs.each {
                 File out = outputProvider.getContentLocation(it.name, it.contentTypes, it.scopes, Format.JAR)
                 FileUtils.copyFile(it.file, out)
-                classPool.appendClassPath(out.path)
+                classPaths.add(classPool.appendClassPath(out.path))
             }
             it.directoryInputs.each {
                 File out = outputProvider.getContentLocation(it.name, it.contentTypes, it.scopes, Format.DIRECTORY)
                 FileUtils.copyDirectory(it.file, out)
-                classPool.insertClassPath(out.path)
+                classPaths.add(classPool.insertClassPath(out.path))
                 handleDirectory(out)
-                loggerWrapper.error("output is " + out.absolutePath)
             }
         }
+        classPaths.each {
+            classPool.removeClassPath(it)
+        }
+        classPool = null
     }
 
-    def handleDirectory(File input){
-        logClassMap.append ("directory ->"+ input.absolutePath +"\n")
+    def handleDirectory(File input) {
         input.eachFileRecurse {
-            if(!it.absolutePath.endsWith(".class")){
+            if (!it.absolutePath.endsWith(".class")) {
                 return
             }
-            def className = it.absolutePath.substring(input.absolutePath.length()+1).replace("\\",".").replace(".class","")
-            CtClass ctClass
-            try {
-                ctClass = classPool.getCtClass(className)
-            }
-            catch (Exception e){
+            def className = it.absolutePath.substring(input.absolutePath.length() + 1).replace("\\", ".").replace(".class", "")
+            CtClass ctClass = classPool.getOrNull(className)
+            if (shouldIgnoreClass(ctClass)) {
                 return
             }
-            if(shouldIgnore(ctClass)){
-                return
-            }
-            logClassMap.append ("class ->"+ input.absolutePath + " " + className + "\n")
+            logClassMap.append("class ->" + input.absolutePath + " " + className + "\n")
             ctClass.getDeclaredMethods().each {
-
-                if(it.empty){
-                    loggerWrapper.error("method empty" + it.name )
-                    return
-                }
-                if((it.modifiers & AccessFlag.NATIVE) != 0){
-                    loggerWrapper.error("method native" + it.name )
+                if (shouldIgnoreMethod(it)) {
                     return
                 }
                 String log = "android.util.Log.i(\"Method_Log\",\"${ctClass.name}->${it.name}\");"
                 it.insertBefore(log)
-//                loggerWrapper.error("class "+  ctClass.name)
             }
-//            ctClass.writeFile()
             ctClass.writeFile(input.absolutePath + File.separator)
         }
     }
 
-    def shouldIgnore(CtClass ctClass){
-        if(ctClass.interface || ctClass.isFrozen()){
+    def shouldIgnoreClass(CtClass ctClass) {
+        if(ctClass == null){
             return true
         }
-        if(!ctClass.declaredMethods || ctClass.declaredMethods.length < 1){
+        if (ctClass.interface || ctClass.isFrozen()) {
+            return true
+        }
+        if (!ctClass.declaredMethods || ctClass.declaredMethods.length < 1) {
             return true
         }
         return false
     }
 
+    def shouldIgnoreMethod(CtMethod ctMethod) {
+        if (ctMethod.empty) {
+            return true
+        }
+        if ((ctMethod.modifiers & AccessFlag.NATIVE) != 0) {
+            return true
+        }
+        return false
+    }
 
     @Override
     boolean isIncremental() {
